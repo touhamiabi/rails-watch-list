dae204e6022c966d5e8eecc4c7c827455b217f45
touhami abi <abitouhami26@gmail.com>
2022-10-25 21:25:31 -0400

    newest commit

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,23 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
 
+  private
 
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 
 end
+
+# <ActionController::Parameters {"authenticity_token"=>"GhmPzH2VvwrqaqeRR0OLXR__SNEgdVn3cnkdkCYyXm8D7QB1Rni8DffJ48j1Rs0I9B3IsdvyUMlzE_MgPflyaw", "bookmark"=>{"movie_id"=>"17", "comment"=>"great!"}, "commit"=>"Create Bookmark", "controller"=>"bookmarks", "action"=>"create", "list_id"=>"1"} permitted: false>
