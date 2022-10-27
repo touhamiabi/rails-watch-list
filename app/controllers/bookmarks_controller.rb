3d76cad68eef45579a7841c44982ae9c7ab3023f
touhami abi <abitouhami26@gmail.com>
2022-10-27 19:54:21 -0400

    latest commit

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,5 +19,3 @@ class BookmarksController < ApplicationController
   end
 
 end
-
-# <ActionController::Parameters {"authenticity_token"=>"GhmPzH2VvwrqaqeRR0OLXR__SNEgdVn3cnkdkCYyXm8D7QB1Rni8DffJ48j1Rs0I9B3IsdvyUMlzE_MgPflyaw", "bookmark"=>{"movie_id"=>"17", "comment"=>"great!"}, "commit"=>"Create Bookmark", "controller"=>"bookmarks", "action"=>"create", "list_id"=>"1"} permitted: false>
